420ce9a1d2856de7b8dac3449cb266e1996ee285
Daria Dasha <dariadasha@MacBook-Air-Daria.local>
2019-09-28 15:48:11 +0300

    dump check number

--- srcs/flags.c
+++ srcs/flags.c
@@ -52,11 +52,19 @@ void		dump_flag_size(t_vm *vm)
 static int	read_flag_d(t_vm *vm, int *ac, char ***av)
 {
 	if (!ft_unsignint((*av)[1]))
-		return (-1);
+	{
+		ft_printf(R"ERROR: number of cycle CAN`T be less than 1\n"OF);
+		vm_free(vm);
+	}
 	else
 	{
 		vm->dump_using = !vm->take_v;
-		vm->cycle_dump = help_atoi((*av)[1]);
+		vm->cycle_dump = help_atoi((*av)[1]);//
+		if (vm->cycle_dump == 0)
+		{
+			ft_printf(R"ERROR: number of cycle CAN`T be less than 1\n"OF);
+			vm_free(vm);
+		}
 		(*av) += 2;
 		(*ac) -= 2;
 	}
